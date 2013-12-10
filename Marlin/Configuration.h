0cd6921cbba3e4639376d5677a85a5d41960ed67
Ronald Miloh Alexander <miloh@froggytoad.net>
2013-12-10 09:47:01 -0800

    increased Z feedrate for vert

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -374,8 +374,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {128.2/2,128.2,6400/6.35,212}  // default steps per unit for Series 1 Vert 
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 12.5, 160}    // (mm/sec)
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {128.2/2,128.2,6400/6.35,212}  // default steps per unit for 2014 Series 1 (2GT belt Beta1)
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 50, 160}    // (mm/sec) higher Z feedrate for vert leadscrew
 #define DEFAULT_MAX_ACCELERATION      {800,800,15,20000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
                                                      
 #define DEFAULT_ACCELERATION          400    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
