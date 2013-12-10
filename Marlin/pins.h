2dcd81fa4edfd7d0ea538c579847822a4838ad08
Ronald Miloh Alexander <miloh@froggytoad.net>
2013-12-10 11:08:34 -0800

    changes to pins.h for lcd implementation.  encrot/click encode broken

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -562,12 +562,12 @@
   #ifdef ULTRA_LCD
 
     #ifdef NEWPANEL
-      #define LCD_PINS_RS 16 
-      #define LCD_PINS_ENABLE 17
-      #define LCD_PINS_D4 23
-      #define LCD_PINS_D5 25 
-      #define LCD_PINS_D6 27
-      #define LCD_PINS_D7 29
+      #define LCD_PINS_RS 32 
+      #define LCD_PINS_ENABLE 47
+      #define LCD_PINS_D4 45
+      #define LCD_PINS_D5 43 
+      #define LCD_PINS_D6 41
+      #define LCD_PINS_D7 39
 
       #ifdef REPRAP_DISCOUNT_SMART_CONTROLLER
         #define BEEPER 37
