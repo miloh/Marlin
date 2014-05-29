abf461e3be6a0f5ec758c15fed62a500033841f9
Ubuntu User <ubuntu@series1-1000.(none)>
2014-05-29 03:52:49 +0000

    fix for the beta 20 machines with 32x microstepping stepper motor drivers

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -374,7 +374,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,800,137.5}  // default steps per unit for 2014 Series 1 (2GT belt Beta1)
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {160,160,1600, 275}  // default steps per unit for 2014 Series 1 (2GT belt Beta1)
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 12.5, 160}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {800,800,15,20000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
                                                      
