6e8b497a8e536b1a2fb516cc5805aca4d21cc12a
R. Miloh Alexander <miloh@froggytoad.net>
2015-08-07 20:45:19 -0700

    ultimachine RAMPS 1.4 have dual headers on Z, machines with single driver dual motor Y axis flip Y and Z for connections

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -599,17 +599,17 @@
     #define X_MIN_PIN           3
     #define X_MAX_PIN           2
 
-    #define Y_STEP_PIN         60
-    #define Y_DIR_PIN          61
-    #define Y_ENABLE_PIN       56
-    #define Y_MIN_PIN          14
-    #define Y_MAX_PIN          15
+    #define Z_STEP_PIN         60
+    #define Z_DIR_PIN          61
+    #define Z_ENABLE_PIN       56
+    #define Z_MIN_PIN          14
+    #define Z_MAX_PIN          15
 
-    #define Z_STEP_PIN         46
-    #define Z_DIR_PIN          48
-    #define Z_ENABLE_PIN       62
-    #define Z_MIN_PIN          18
-    #define Z_MAX_PIN          19
+    #define Y_STEP_PIN         46
+    #define Y_DIR_PIN          48
+    #define Y_ENABLE_PIN       62
+    #define Y_MIN_PIN          18
+    #define Y_MAX_PIN          19
 
     #define Y2_STEP_PIN        36
     #define Y2_DIR_PIN         34
